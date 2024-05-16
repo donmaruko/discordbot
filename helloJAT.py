f3b22a057d322aea4cf46e4efb5a63d55a53fd65
Don <78839918+donmaruko@users.noreply.github.com>
2024-05-16 19:40:56 -0400

    Delete helloJAT.py

--- helloJAT.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-@commands.command(name='hello', help='The classic greeting command!')
-async def hello(ctx):
-    await ctx.send(f'Hey, {ctx.author.mention}!')
-
-def setup(bot):
-    bot.add_command(hello)
